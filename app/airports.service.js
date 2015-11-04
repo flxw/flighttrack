2de3b2b2be2ac5c3d01dff0016761a61f97a0c8d
Felix Wolff <felix.stefan.wolff@gmail.com>
2015-11-04 10:01:09 +1000

    Finish editing mode of flights

---
+++ app/airports.service.js
@@ -0,0 +1,20 @@
+(function() {
+// ------------------------------------
+
+angular
+  .module('myApp')
+  .factory('airports', airports);
+
+function airports() {
+  return [
+    { code: 'FRA', city: 'Frankfurt', coordinates: { latitude:  50.036512, longitude: 8.558235 } },
+    { code:'DXB', city: 'Dubai', coordinates: { latitude: 25.253834, longitude: 55.364814 } },
+    { code: 'BNE', city: 'Brisbane', coordinates: { latitude: -27.394051, longitude: 153.121133 } },
+    { city: "Sydney", code: "SYD", coordinates: { latitude: -33.946111, longitude: 151.177222 } },
+    { city: "Cairns", code: "CNS", coordinates: { latitude: -16.885833, longitude: 145.755278 } },
+    { city: "Shanghai", code: "PVG", coordinates: { latitude: 31.143378, longitude: 121.805214 } },
+    { city: "Cologne", code: "CGN", coordinates: { latitude: 50.865917, longitude: 7.142744 } }
+  ]
+}
+// ------------------------------------
+})()
--- app/components/flight-list-item.directive.js
+++ app/components/flight-list-item.directive.js
@@ -17,21 +17,34 @@ function flightListItemDirective() {
   }
 }
 
-function flightListItemCtrl($scope) {
+flightListItemCtrl.$inject = ["$scope", "airports"];
+
+function flightListItemCtrl($scope, airports) {
   var vm = this;
 
   vm.isEditing = false;
-  vm.origin = $scope.flight.origin
-  vm.destination = $scope.flight.destination
-  vm.airline = $scope.flight.airline
-  vm.airports = [
-    { name: "Kuala Lumpur", code: "KLX" },
-    { name: "Frankfurt am Main", code: "FRA" }
-  ]
-  vm.departureTime = ''
-  vm.arrivalTime = ''
+  vm.origin = $scope.flight.origin.city + " (" + $scope.flight.origin.code + ")";
+  vm.destination = $scope.flight.destination.city + " (" + $scope.flight.destination.code + ")";
+  vm.airports = airports;
+  vm.departureTimestamp = $scope.flight.departureTimestamp;
+  vm.arrivalTimestamp = $scope.flight.arrivalTimestamp;
+  vm.airline = $scope.flight.airline;
 
   vm.startEdit   = function() { vm.isEditing = true };
-  vm.saveChanges = function() { vm.isEditing = false };
+  vm.saveChanges = function() {
+    vm.isEditing = false;
+
+    // just get the airport code here
+    $scope.flight.origin.city = vm.origin.substring(0, vm.origin.length-6) // remove outer braces
+    $scope.flight.origin.code = vm.origin.substring(vm.origin.length-4, vm.origin.length-1) // remove outer braces
+
+    $scope.flight.destination.city = vm.destination.substring(0, vm.destination.length-6) // remove outer braces
+    $scope.flight.destination.code = vm.destination.substring(vm.destination.length-4, vm.destination.length-1) // remove outer braces
+
+    $scope.flight.departureTimestamp = vm.departureTimestamp;
+    $scope.flight.arrivalTimestamp = vm.arrivalTimestamp;
+
+    $scope.flight.airline = vm.airline
+  };
 }
 })()
