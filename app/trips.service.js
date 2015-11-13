7a9acbc0d6d5c4c8491f8a20649ae790050b906b
Felix Wolff <felix.stefan.wolff@gmail.com>
2015-11-13 14:53:25 +1000

    Improve styling of trip text

--- app/trips.service.js
+++ app/trips.service.js
@@ -11,9 +11,19 @@ angular
         destination: { name: 'Dubai', coordinates: { latitude: 25.253834, longitude: 55.364814 } },
         startDate: new Date(2015, 7, 14),
         endDate: new Date(2015, 7, 15),
-        storyMarkdown:  "![](https://raw.githubusercontent.com/flxw/scplay/master/images/playing2.png)\n" +
-        "# Running it\n" +
-        "Windows Users will find builds attached to the releases above.\n" +
+        storyMarkdown:  "# Running it\n" +
+
+        "I tried to make scplay as easy to use as possible. When firing it up for the first time, you are asked for your\n" +
+        "permalink profile ID, which you can extract from the permalink to you profile. My profile ID would be:\n" +
+        "*http:// soundcloud.com/<span style='color:#f00'>fe-lix-62</span>*\n" +
+        "![](https://raw.githubusercontent.com/flxw/scplay/master/images/playing2.png)\n" +
+        "From there on, simply double-click your songs inside the list to play them. The application\n" +
+        "will hide itself once you click elsewhere on the screen. To bring it back up, click on the icon in the systray.\n" +
+        "![Initial scplay screen](https://github.com/flxw/scplay/blob/master/images/initial.png)\n" +
+        "### Running it\n" +
+        "#### Running it\n" +
+        "##### Running it\n" +
+        "**Windows** *Users* will find builds attached to the releases above.\n" +
         "Those with another OS will need to build it themselves\n" +
         "``` bash\n" +
         "git clone http://github.com/flxw/scplay /tmp/scplay\n" +
@@ -21,13 +31,6 @@ angular
         "qmake\n" +
         "make\n" +
         "```\n" +
-        "# Usage\n" +
-        "I tried to make scplay as easy to use as possible. When firing it up for the first time, you are asked for your\n" +
-        "permalink profile ID, which you can extract from the permalink to you profile. My profile ID would be:\n" +
-        "*http:// soundcloud.com/<span style='color:#f00'>fe-lix-62</span>*\n" +
-        "From there on, simply double-click your songs inside the list to play them. The application\n" +
-        "will hide itself once you click elsewhere on the screen. To bring it back up, click on the icon in the systray.\n" +
-        "![Initial scplay screen](https://github.com/flxw/scplay/blob/master/images/initial.png)\n" +
         "## Version\n" +
         "To find out your version number, right-click the icon in your systray. Always be sure to make use of the latest one ;)\n" +
         "![finding out your version-number](https://github.com/flxw/scplay/blob/master/images/finding-version-number.png)\n" +
